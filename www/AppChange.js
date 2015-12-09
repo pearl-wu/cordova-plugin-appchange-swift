7e4b92a33a52f25752526e87a6be0c9805754b6d
pearl <lu92500@yahoo.com.tw>
2015-12-09 14:08:08 +0800

    Update AppChange.js

--- www/AppChange.js
+++ www/AppChange.js
@@ -5,7 +5,7 @@ var exec = require("cordova/exec");
 var Hello = function(){};
 
 Hello.prototype.check = function (uri, id successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "AppChange", "check, [uri, id]);
+    cordova.exec(successCallback, errorCallback, "AppChange", "check", [uri, id]);
 };
 Hello.prototype.GOapprul = function (Scheme, successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "AppChange", "goAPPurl", Scheme]);
