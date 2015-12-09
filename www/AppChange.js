75e64d38cc30023ca3f1fe5431f64a349668c054
pearl <lu92500@yahoo.com.tw>
2015-12-09 14:07:06 +0800

    Update and rename hello.js to AppChange.js

---
+++ www/AppChange.js
@@ -0,0 +1,20 @@
+/*global cordova*/
+
+var exec = require("cordova/exec");
+
+var Hello = function(){};
+
+Hello.prototype.check = function (uri, id successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "AppChange", "check, [uri, id]);
+};
+Hello.prototype.GOapprul = function (Scheme, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "AppChange", "goAPPurl", Scheme]);
+};
+Hello.prototype.OPENapp = function (Scheme, mgs successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "AppChange", "toAPPopen", [Scheme, mgs]);
+};
+Hello.prototype.INport = function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "AppChange", "Inport", []);
+};
+
+module.exports = new Hello();
